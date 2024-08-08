214038d2d6d3edad181186b5d16f2feb90f39e79
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-08 21:37:59 +0900

    Boot

--- hosts/x1carbon/default.nix
+++ hosts/x1carbon/default.nix
@@ -26,6 +26,7 @@
   # boot.loader.grub.enable = true;
   # boot.loader.grub.device = "/dev/sda";
   # boot.loader.grub.useOSProber = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
