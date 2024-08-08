9b1a877baa2c04039d88ba029d88a82c5f5fed5d
ikazuk <72190609+Ichinose-Kazuki@users.noreply.github.com>
2024-08-08 21:18:06 +0900

    Update default.nix

--- hosts/x1carbon/default.nix
+++ hosts/x1carbon/default.nix
@@ -22,10 +22,10 @@
     ];
   };
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  # Bootloader. (Maybe managed by disko?)
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/sda";
+  # boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
