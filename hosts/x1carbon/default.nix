f0d212630dabbb6424941b08ba6e54e513ea84e3
ikazuk <72190609+Ichinose-Kazuki@users.noreply.github.com>
2024-08-08 20:30:45 +0900

    Update default.nix

--- hosts/x1carbon/default.nix
+++ hosts/x1carbon/default.nix
@@ -90,7 +90,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # sound.enable = true; Deprecated
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
