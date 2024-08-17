9e8df389783597e692b072eb3869b8458732af7d
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-18 02:00:45 +0900

    Add index number to diskoTest

--- hosts/diskoTest/disko.nix
+++ hosts/diskoTest/disko.nix
@@ -10,6 +10,7 @@
             ESP = {
               type = "EF00";
               size = "250M";
+              _index = 1;
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -20,6 +21,7 @@
             protect = {
               type = "8300";
               size = "10M";
+              _index = 2;
               content = {
                 type = "filesystem";
                 format = "ext4";
@@ -29,6 +31,7 @@
             root = {
               type = "8300";
               size = "100%";
+              _index = 3;
               content = {
                 type = "filesystem";
                 format = "ext4";
