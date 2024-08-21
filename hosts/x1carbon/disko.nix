a8e38b00f2c4e17b4640ab50c822ddbb64248107
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-22 03:09:06 +0900

    Add full disk setup in disko.nix

--- hosts/x1carbon/disko.nix
+++ hosts/x1carbon/disko.nix
@@ -59,12 +59,13 @@
                   # if you want to use the key for interactive login be sure there is no trailing newline
                   # for example use `echo -n "password" > /tmp/secret.key`
                   keyFile = "/tmp/secret.key";
+                  # Enable SSD TRIM command
+                  # https://wiki.archlinux.org/title/Dm-crypt/Specialties#Discard/TRIM_support_for_solid_state_drives_(SSD)
                   allowDiscards = true;
                 };
-                additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
                 content = {
                   type = "lvm_pv";
-                  vg = "pool";
+                  vg = "MyVolGroup";
                 };
               };
             };
@@ -76,28 +77,59 @@
       pool = {
         type = "lvm_vg";
         lvs = {
-          root = {
-            size = "100M";
+          root = let
+              # In the current btrfs implementation, all subvolumes share the same options.
+              rootBtrfsOptions = [ "compress=zstd:8" "noatime" "enospc_debug" ];
+            in
+            {
+            size = "100G";
             content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-              mountOptions = [
-                "defaults"
-              ];
+              type = "btrfs";
+              extraArgs = [ "-f" ]; # Override existing partition
+              # Subvolumes must set a mountpoint in order to be mounted,
+              # unless their parent is mounted
+              subvolumes = {
+                "/rootfs" = {
+                  mountpoint = "/";
+                  mountOptions = rootBtrfsOptions;
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = rootBtrfsOptions;
+                };
+              };
             };
           };
-          home = {
-            size = "10M";
+          home = let
+              # In the current btrfs implementation, all subvolumes share the same options.
+              homeBtrfsOptions = [ "compress=zstd:3" "noatime" "enospc_debug" "usebackuproot" ];
+            in
+            {
+            size = "150G";
             content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/home";
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/home" = {
+                  mountpoint = "/home";
+                  mountOptions = homeBtrfsOptions;
+                };
+                # Run `btrfs subvolume snapshot /home /snapshots/home/xxxx-xx-xx */` to create a snapshot.
+                "/snapshots" = {
+                  mountpoint = "/snapshots";
+                  mountOptions = homeBtrfsOptions;
+                };
+              };
             };
           };
-          raw = {
-            size = "10M";
-          };
+          encryptedSwap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority = 100; # prefer to encrypt as long as we have space for it
+              };
+            };
         };
       };
     };
