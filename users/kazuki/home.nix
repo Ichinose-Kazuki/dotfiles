9d98057288c1cc9b6d38c4f2ed1d06d667045fdf
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-23 03:08:57 +0900

    Fix .ssh/config problem in vscode-fhs.
    https://github.com/nix-community/home-manager/issues/322#issuecomment-2254185770

--- users/kazuki/home.nix
+++ users/kazuki/home.nix
@@ -71,6 +71,11 @@ in
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+
+    ".ssh/config" = {
+      target = ".ssh/config_source";
+      onChange = ''cat .ssh/config_source > .ssh/config && chmod 400 .ssh/config'';
+    };
   };
 
   # Home Manager can also manage your environment variables through
