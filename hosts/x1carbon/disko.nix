19e6b77833b9c99d8ea3705a6fcc4c6f9f8ee03d
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-23 01:35:17 +0900

    Update disko.nix

--- hosts/x1carbon/disko.nix
+++ hosts/x1carbon/disko.nix
@@ -7,7 +7,8 @@
         content = {
           type = "gpt";
           partitions = {
-            p1_ESP = { # Partitions are sorted in alphabetical order?
+            p1_ESP = {
+              # Partitions are sorted in alphabetical order?
               type = "EF00";
               size = "260M";
               content = {
@@ -69,60 +70,62 @@
       MyVolGroup = {
         type = "lvm_vg";
         lvs = {
-          root = let
+          root =
+            let
               # In the current btrfs implementation, all subvolumes share the same options.
               rootBtrfsOptions = [ "compress=zstd:8" "noatime" "enospc_debug" ];
             in
             {
-            size = "100G";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ]; # Override existing partition
-              # Subvolumes must set a mountpoint in order to be mounted,
-              # unless their parent is mounted
-              subvolumes = {
-                "/rootfs" = {
-                  mountpoint = "/";
-                  mountOptions = rootBtrfsOptions;
-                };
-                "/nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = rootBtrfsOptions;
+              size = "100G";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  "/" = {
+                    mountpoint = "/";
+                    mountOptions = rootBtrfsOptions;
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = rootBtrfsOptions;
+                  };
                 };
               };
             };
-          };
-          home = let
+          home =
+            let
               # In the current btrfs implementation, all subvolumes share the same options.
               # LZO is better on nvme-ssd: https://gist.github.com/braindevices/fde49c6a8f6b9aaf563fb977562aafec
               homeBtrfsOptions = [ "compress=lzo" "noatime" "enospc_debug" "usebackuproot" ];
             in
             {
-            size = "150G";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ];
-              subvolumes = {
-                "/home" = {
-                  mountpoint = "/home";
-                  mountOptions = homeBtrfsOptions;
-                };
-                # Run `btrfs subvolume snapshot /home /snapshots/home/xxxx-xx-xx */` to create a snapshot.
-                "/snapshots" = {
-                  mountpoint = "/snapshots";
-                  mountOptions = homeBtrfsOptions;
+              size = "150G";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/home" = {
+                    mountpoint = "/home";
+                    mountOptions = homeBtrfsOptions;
+                  };
+                  # Run `btrfs subvolume snapshot /home /snapshots/home/xxxx-xx-xx` to create a snapshot.
+                  "/snapshots" = {
+                    mountpoint = "/snapshots";
+                    mountOptions = homeBtrfsOptions;
+                  };
                 };
               };
+            };
+          encryptedSwap = {
+            size = "100%FREE";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+              priority = 100; # prefer to encrypt as long as we have space for it
             };
           };
-          encryptedSwap = {
-              size = "100%FREE";
-              content = {
-                type = "swap";
-                randomEncryption = true;
-                priority = 100; # prefer to encrypt as long as we have space for it
-              };
-            };
         };
       };
     };
