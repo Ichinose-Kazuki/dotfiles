1f9ab76b42894ce231ec2ae90500e1bc73f4d616
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-24 02:56:15 +0900

    Fix keyboard config

--- modules/nixos/x1carbon/udev/default.nix
+++ modules/nixos/x1carbon/udev/default.nix
@@ -1,10 +1,11 @@
 {
+  # Comments are allowed only on the top level.
   services.udev.extraHwdb = ''
-    evdev:atkbd:*
     # Disable PgUp(c9) and PgDn(d1) keys.
-    KEYBOARD_KEY_c9=reserved
-    KEYBOARD_KEY_d1=reserved
     # Change CapsLock to Ctrl.
-    KEYBOARD_KEY_3a=leftctrl
+    evdev:atkbd:*
+      KEYBOARD_KEY_c9=reserved
+      KEYBOARD_KEY_d1=reserved
+      KEYBOARD_KEY_3a=leftctrl
   '';
 }
