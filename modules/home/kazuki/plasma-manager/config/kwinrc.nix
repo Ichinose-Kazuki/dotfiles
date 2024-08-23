00ca867e6117f1602243a43da577d92371b2539d
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-24 00:27:51 +0900

    Disable default screen edge

--- modules/home/kazuki/plasma-manager/config/kwinrc.nix
+++ modules/home/kazuki/plasma-manager/config/kwinrc.nix
@@ -1,6 +1,11 @@
 {
-  kwinrc.Wayland."InputMethod" = {
-    shellExpand = true;
-    value = "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
+  kwinrc = {
+    Wayland."InputMethod" = {
+      shellExpand = true;
+      value = "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
+    };
+    "Effect-overview" = {
+      BorderActivate = "9";
+    };
   };
 }
