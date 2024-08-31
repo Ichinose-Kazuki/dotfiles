e369fad6f2ea3f0c72bd0d3b5494f4e08bf34223
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-08-31 17:52:57 +0900

    Change hostname

--- hosts/x1carbon/default.nix
+++ hosts/x1carbon/default.nix
@@ -51,7 +51,7 @@
   };
   # boot.loader.efi.canTouchEfiVariables = true; # Required to write boot entry to NVRAM.
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "programming"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
