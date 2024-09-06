ea078f8693d33a0d37fc370ec73bcf1e781080db
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-09-07 04:10:45 +0900

    Add config for raspberry-pi-nix

--- flake.nix
+++ flake.nix
@@ -103,26 +103,62 @@
         ];
       };
 
-      nixosConfigurations.raspi3bp = nixpkgs.lib.nixosSystem {
-        # Note that you cannot put arbitrary configuration here: the configuration must be placed in the files loaded via modules
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          nixos-hardware.nixosModules.raspberry-pi-3
-          ./hosts/raspi3bp
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.kazuki = import ./users/kazuki/home_raspi.nix;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              host = "raspi3bp";
+      nixosConfigurations.raspi3bp =
+        let
+          basic-config = { pkgs, lib, ... }: {
+            # bcm2711 for rpi 3, 3+, 4, zero 2 w
+            # bcm2712 for rpi 5
+            # See the docs at:
+            # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
+            raspberry-pi-nix.board = "bcm2711";
+            networking = {
+              useDHCP = false;
+              interfaces = {
+                # wlan0.useDHCP = true;
+                eth0.useDHCP = true;
+              };
             };
-          }
-        ];
-      };
+            hardware = {
+              # bluetooth.enable = true;
+              raspberry-pi = {
+                config = {
+                  all = {
+                    base-dt-params = {
+                      # enable autoprobing of bluetooth driver
+                      # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
+                      # krnbt = {
+                      #   enable = true;
+                      #   value = "on";
+                      # };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        in
+        nixpkgs.lib.nixosSystem {
+          # Note that you cannot put arbitrary configuration here: the configuration must be placed in the files loaded via modules
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            nixos-hardware.nixosModules.raspberry-pi-3
+            raspberry-pi-nix.nixosModules.raspberry-pi
+            basic-config
+            ./hosts/raspi3bp
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.kazuki = import ./users/kazuki/home_raspi.nix;
+              home-manager.extraSpecialArgs = {
+                inherit inputs;
+                host = "raspi3bp";
+              };
+            }
+          ];
+        };
 
       nixosConfigurations.diskoTest = nixpkgs.lib.nixosSystem {
         # Note that you cannot put arbitrary configuration here: the configuration must be placed in the files loaded via modules
--- hosts/raspi3bp/default.nix
+++ hosts/raspi3bp/default.nix
@@ -2,12 +2,11 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
+{ config
+, lib
+, pkgs
+, inputs
+, ...
 }:
 
 {
@@ -22,7 +21,8 @@
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
   # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
+  # Avoid conflict between raspberry-pi-nix and nixos-hardware.
+  boot.loader.generic-extlinux-compatible.enable = lib.mkForce false;
 
   networking.hostName = "raspi3bp"; # Define your hostname.
   # Pick only one of the below networking options.
