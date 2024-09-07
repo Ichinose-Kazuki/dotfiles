42b0d73c804d54a1293a4330c720fd4fda36f455
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-09-08 03:35:36 +0900

    raspi3bp: disable NetworkManager

--- hosts/raspi3bp/default.nix
+++ hosts/raspi3bp/default.nix
@@ -65,7 +65,8 @@
   networking.hostName = "raspi3bp"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  # NetworkManager comes with so many unwanted softwares. Don't enable it.
+  # networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
