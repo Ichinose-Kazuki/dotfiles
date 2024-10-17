aa1f38fa85e48165f43492862e033431be6696b9
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-10-18 00:34:33 +0900

    common: add comment

--- modules/nixos/common/compatibility/default.nix
+++ modules/nixos/common/compatibility/default.nix
@@ -4,7 +4,7 @@
 }:
 
 {
-  services.envfs.enable = true;
+  services.envfs.enable = true; # Useful to execute shebangs on NixOS that assume hard coded locations in locations like /bin or /usr/bin
 
   programs.nix-ld = { # Necessary on vscode remote host and to run unpatched binaries.
     enable = true;
