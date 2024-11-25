8aaac5041b33be25eba6c9e602fa727e0ee9ca60
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-11-26 02:55:37 +0900

    raspi3bp: tailscale optimization

--- modules/nixos/raspi3bp/tailscale/default.nix
+++ modules/nixos/raspi3bp/tailscale/default.nix
@@ -1,3 +1,5 @@
+{ lib, pkgs, ... }:
+
 {
   services.tailscale = {
     enable = true;
@@ -8,6 +10,25 @@
       "--advertise-routes=192.168.11.0/24"
       "--advertise-exit-node"
     ];
+  };
+
+  # Linux optimizations for subnet routers and exit nodes
+  # https://tailscale.com/kb/1320/performance-best-practices#linux-optimizations-for-subnet-routers-and-exit-nodes
+  environment.systemPackages = with pkgs; [
+    ethtool
+  ];
+  # tx-udp-segmentation described in https://tailscale.com/blog/more-throughput is not supported by the Raspberry Pi 3B+.
+  services.networkd-dispatcher = {
+    enable = true;
+    rules = {
+      "50-tailscale" = {
+        onState = [ "routable" ];
+        script = ''
+          #!${pkgs.runtimeShell}
+          ${lib.getExe pkgs.ethtool} -K eth0 rx-udp-gro-forwarding on rx-gro-list off
+        '';
+      };
+    };
   };
 
   # Make tailscale state information persistent.
