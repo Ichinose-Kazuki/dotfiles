a594d99c7635629c19bb571cef3d2c2284a49908
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-11-26 02:16:10 +0900

    raspi3bp: watch.py successfully power on tsuyoServer

--- modules/nixos/raspi3bp/boot-shutdown-button/watch.py
+++ modules/nixos/raspi3bp/boot-shutdown-button/watch.py
@@ -5,9 +5,33 @@
 """Minimal example of watching for edges on a single line."""
 
 import gpiod
+import subprocess
 
 from datetime import timedelta
 from gpiod.line import Bias, Edge
+
+def check_server_status(ip_address='192.168.11.7'):
+    # Run the ping command with a timeout of 1 second and 1 packet
+    result = subprocess.run(
+        ['ping', ip_address, '-c', '1', '-w', '1'], 
+    )
+    # Check if the command was successful
+    if result.returncode == 0:
+        return True
+    else:
+        return False
+    
+    
+def shutdown_server():
+    print('Shut down is not supported yet')
+    
+
+def  power_on_server(mac_address='50:eb:f6:b9:16:8a'):
+    # Send a Wake-on-LAN packet to the server
+    print('Powering on the server')
+    subprocess.run(
+        ['wol', mac_address],
+    )
 
 
 def edge_type_str(event):
@@ -40,10 +64,28 @@ def watch_line_value(chip_path, line_offset):
                         event.line_offset, edge_type_str(event), event.line_seqno
                     )
                 )
-
+                is_powered_on = check_server_status()
+                if (is_powered_on):
+                    print('Server is powered on')
+                    if event.event_type is event.Type.FALLING_EDGE:
+                        falling_edge_time = event.timestamp_ns
+                    else:
+                        rising_edge_time = event.timestamp_ns
+                        if 'falling_edge_time' in locals() \
+                            and (rising_edge_time - falling_edge_time) >= 1 * 1e9:
+                            shutdown_server()
+                        else:
+                            print('Press and hold the button for at least 1 second to shut down the server')
+                else:
+                    print('Server is powered off')
+                    if event.event_type is event.Type.FALLING_EDGE:
+                        power_on_server()
+                    else:
+                        print('Press the button to power on the server')
+                
 
 if __name__ == "__main__":
     try:
-        watch_line_value("/dev/gpiochip0", 27)
+        watch_line_value("/dev/gpiochip0", 5)
     except OSError as ex:
         print(ex, "\nCustomise the example configuration to suit your situation")
