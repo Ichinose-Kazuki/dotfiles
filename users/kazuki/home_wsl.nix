d33a7969178b66afcb1929cd404f824d3d7de149
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2024-11-26 18:34:19 +0900

    standalone hm setting for wsl ubuntu

--- users/kazuki/home_wsl.nix
+++ users/kazuki/home_wsl.nix
@@ -99,10 +99,14 @@
     };
     direnv = {
       enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
       nix-direnv.enable = true;
     };
     bash = {
       enable = true;
+      profileExtra = "source ${config.home.homeDirectory}/.profile.manual";
+      initExtra = "source ${config.home.homeDirectory}/.bashrc.manual";
     };
     carapace.enable = true;
     zsh = {
@@ -127,14 +131,57 @@
           l = "ls";
         }
       ];
+      history.size = 10000000;
+      # Copied from the following URL and modified:
+      # https://discourse.nixos.org/t/using-zsh-with-grml-config-and-nix-shell-prompt-indicator/13838 
+      initExtraFirst = ''
+        # Note that loading grml's zshrc here will override NixOS settings such as
+        # `programs.zsh.histSize`, so they will have to be set again below.
+        source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
+
+        # Increase history size.
+        HISTSIZE=${builtins.toString config.programs.zsh.history.size}
+
+        # Prompt modifications.
+        #
+        # In current grml zshrc, changing `$PROMPT` no longer works,
+        # and `zstyle` is used instead, see:
+        # https://unix.stackexchange.com/questions/656152/why-does-setting-prompt-have-no-effect-in-grmls-zshrc
+
+        # Disable the grml `sad-smiley` on the right for exit codes != 0;
+        # it makes copy-pasting out terminal output difficult.
+        # Done by setting the `items` of the right-side setup to the empty list
+        # (as of writing, the default is `items sad-smiley`).
+        # See: https://bts.grml.org/grml/issue2267
+        zstyle ':prompt:grml:right:setup' items
+
+        # Add nix-shell indicator that makes clear when we're in nix-shell.
+        # Set the prompt items to include it in addition to the defaults:
+        # Described in: http://bewatermyfriend.org/p/2013/003/
+        function nix_shell_prompt () {
+          REPLY=''${IN_NIX_SHELL+"(nix-shell) "}
+        }
+        grml_theme_add_token nix-shell-indicator -f nix_shell_prompt '%F{magenta}' '%f'
+
+        # grml prompt customization with "zstyle" command
+        # To see available options: $ prompt -h grml
+        zstyle ':prompt:grml:left:items:path' pre ' %F{245}'
+        zstyle ':prompt:grml:left:items:path' post '%f'
+        zstyle ':prompt:grml:left:items:percent' pre '%F{245}'
+        zstyle ':prompt:grml:left:items:percent' post '%f'
+        zstyle ':prompt:grml:left:setup' items rc nix-shell-indicator change-root user path vcs percent
+
+        # Set locale to English
+        LANG=en_US.UTF-8
+      '';
     };
   };
 
   # Internationalization
-  i18n = {
-    glibcLocales = pkgs.glibcLocales.override {
-      allLocales = false;
-      locales = [ "ja_JP.UTF-8/UTF-8" ];
-    };
-  };
+  # i18n = {
+  #   glibcLocales = pkgs.glibcLocales.override {
+  #     allLocales = false;
+  #     locales = [ "ja_JP.UTF-8/UTF-8" ];
+  #   };
+  # };
 }
