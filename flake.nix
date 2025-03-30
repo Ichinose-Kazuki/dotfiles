7534472ce6198a756606cd493e30958b18ca0fd7
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2025-03-30 14:00:52 +0900

    add hyprland.cachix and ags.cachix as extra-substituters

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://ags.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
     ];
   };
 
