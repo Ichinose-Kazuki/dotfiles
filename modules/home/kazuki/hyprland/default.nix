a65f3d42ce14499f2d66c444f2915ab32cd05f4d
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2025-06-17 01:12:08 +0900

    Configure 'Variables' and some essential keybinds in hyprland.

--- modules/home/kazuki/hyprland/default.nix
+++ modules/home/kazuki/hyprland/default.nix
@@ -32,48 +32,25 @@
         "HYPRCURSOR_SIZE,24"
       ];
       # permissions
-      # look and feel
-      general = {
-        gaps_in = 5;
-        gaps_out = 20;
+      # look and feel: https://wiki.hypr.land/Configuring/Variables/
+      general = rec {
         border_size = 2;
+        gaps_in = (8 - border_size) / 2; # 2*gaps_in between windows.
+        gaps_out = (8 - border_size);
         "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
-        resize_on_border = false;
-        allow_tearing = false;
-        layout = "dwindle";
+        layout = "master";
       };
       decoration = {
         rounding = 10;
-        rounding_power = 2;
-        active_opacity = 1.0;
-        inactive_opacity = 1.0;
+        blur.enabled = false;
         shadow = {
-          enabled = true;
-          range = 4;
-          render_power = 3;
-          color = "rgba(1a1a1aee)";
-        };
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-          vibrancy = 0.1696;
+          color = "rgba(1a1a1aaf)";
         };
       };
 
       # https://wiki.hyprland.org/Configuring/Variables/#animations
       animations = {
-        enabled = "yes, please :)";
-
-        bezier = [
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-          "almostLinear,0.5,0.5,0.75,1.0"
-          "quick,0.15,0,0.1,1"
-        ];
-
         animation = [
           "global, 1, 10, default"
           "border, 1, 5.39, easeOutQuint"
@@ -92,30 +69,29 @@
           "workspacesIn, 1, 1.21, almostLinear, fade"
           "workspacesOut, 1, 1.94, almostLinear, fade"
         ];
-      };
-      dwindle = {
-        pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-        preserve_split = true; # You probably want this
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "easeInOutCubic,0.65,0.05,0.36,1"
+          "linear,0,0,1,1"
+          "almostLinear,0.5,0.5,0.75,1.0"
+          "quick,0.15,0,0.1,1"
+        ];
       };
       master = {
-        new_status = "master";
+        mfact = 0.50;
       };
       misc = {
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
+        disable_autoreload = true; # might save on battery.
       };
       # todo: configure window snap for picture-in-picture mode.
       # input
+      # refer to https://wayland-book.com/seat/xkb.html for details of xkb.
       input = {
-        kb_layout = "us";
-        kb_variant = "";
-        kb_model = "";
-        kb_options = "";
-        kb_rules = "";
-
-        follow_mouse = 1;
-
-        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+        repeat_delay = 300;
+        sensitivity = 0.5; # -1.0 - 1.0, 0 means no modification.
+        accel_profile = "adaptive";
 
         touchpad = {
           natural_scroll = false;
@@ -126,27 +102,46 @@
       gestures = {
         workspace_swipe = false;
       };
-      device = {
-        name = "epic-mouse-v1";
-        sensitivity = -0.5;
+      xwayland = {
+        # Less secure but more compatible.
+        # Abstract sockets exist in a kernel-managed namespace independent of the filesystem,
+        # identified by a null byte (\0) prefix in their name.
+        # Many X11 applications prefer abstract sockets for flexibility,
+        # as they bypass filesystem permissions.
+        create_abstract_socket = true;
+      };
+      render = {
+        # direct_scanout = 1; # seems useful, but causes problems with many GPUs.
+      };
+      cursor = {
+        # persistent_warps = true; # probably useful with a track ball mouse.
+      };
+      ecosystem = {
+        # no_donation_nag = true;
+        enforce_permissions = true;
       };
       # keybindings
       "$mainMod" = "ALT";
+      "$appMod" = "CTRL + SHIFT";
       bind = [
-        "$mainMod, Q, exec, $terminal"
-        "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, E, exec, $fileManager"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, R, exec, $menu"
-        "$mainMod, P, pseudo," # dwindle
-        "$mainMod, J, togglesplit," # dwindle
+        "$mainMod + SHIFT, Q, killactive,"
+        "$mainMod, F, fullscreen, 0"
+        # "$mainMod, F, fullscreenstate, 0" # send fullscreen state to client app?
+        "$appMod, T, exec, $terminal"
+        "$appMod, E, exec, $fileManager"
+        "$mainMod, SPACE, exec, $menu"
 
-        # Move focus with mainMod + arrow keys
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
+        # Move focus with mainMod + hjkl
+        "$mainMod, h, movefocus, l"
+        "$mainMod, l, movefocus, r"
+        "$mainMod, k, movefocus, u"
+        "$mainMod, j, movefocus, d"
+
+        # Move active window in a direction with mainMod + SHIFT + hjkl
+        "$mainMod + SHIFT, h, movewindow, l"
+        "$mainMod + SHIFT, l, movewindow, r"
+        "$mainMod + SHIFT, k, movewindow, u"
+        "$mainMod + SHIFT, j, movewindow, d"
 
         # Switch workspaces with mainMod + [0-9]
         "$mainMod, 1, workspace, 1"
@@ -161,24 +156,20 @@
         "$mainMod, 0, workspace, 10"
 
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
+        "$mainMod + SHIFT, 1, movetoworkspace, 1"
+        "$mainMod + SHIFT, 2, movetoworkspace, 2"
+        "$mainMod + SHIFT, 3, movetoworkspace, 3"
+        "$mainMod + SHIFT, 4, movetoworkspace, 4"
+        "$mainMod + SHIFT, 5, movetoworkspace, 5"
+        "$mainMod + SHIFT, 6, movetoworkspace, 6"
+        "$mainMod + SHIFT, 7, movetoworkspace, 7"
+        "$mainMod + SHIFT, 8, movetoworkspace, 8"
+        "$mainMod + SHIFT, 9, movetoworkspace, 9"
+        "$mainMod + SHIFT, 0, movetoworkspace, 10"
 
-        # Example special workspace (scratchpad)
-        "$mainMod, S, togglespecialworkspace, magic"
-        "$mainMod SHIFT, S, movetoworkspace, special:magic"
-
-        # Scroll through existing workspaces with mainMod + scroll
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
+        # # Example special workspace (scratchpad)
+        # "$mainMod, S, togglespecialworkspace, magic"
+        # "$mainMod SHIFT, S, movetoworkspace, special:magic"
       ];
 
       # Move/resize windows with mainMod + LMB/RMB and dragging
