a01363b3a9d332dee41115a02a690b4a43147e74
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2025-07-20 01:29:45 +0900

    Fix errors with hyprlock, install brightnessctl.

--- modules/home/kazuki/hyprland/hypr-ecosystem.nix
+++ modules/home/kazuki/hyprland/hypr-ecosystem.nix
@@ -22,6 +22,8 @@ in
     hyprsysteminfo
     # Qt6 QML provider for hypr* apps
     hyprland-qt-support
+    # idle management
+    brightnessctl
   ];
 
   # wallpaper
@@ -79,37 +81,99 @@ in
   };
 
   # hyprlock
+  # example conf: https://github.com/hyprwm/hyprlock/blob/main/assets/example.conf
   programs.hyprlock = {
     enable = true;
     settings = {
+      "$font" = "Monospace";
+
       general = {
-        disable_loading_bar = true;
-        grace = 300;
-        hide_cursor = true;
-        no_fade_in = false;
+        hide_cursor = false;
+      };
+
+      # auth = {
+      #   fingerprint = {
+      #     enabled = true;
+      #     ready_message = "Scan fingerprint to unlock";
+      #     present_message = "Scanning...";
+      #     retry_delay = 250; # in milliseconds
+      #   };
+      # };
+
+      animations = {
+        enabled = true;
+        bezier = "linear, 1, 1, 0, 0";
+        animation = [
+          "fadeIn, 1, 5, linear"
+          "fadeOut, 1, 5, linear"
+          "inputFieldDots, 1, 2, linear"
+        ];
       };
 
       background = [
         {
+          monitor = "";
           path = wallpaperPath;
           blur_passes = 3;
-          blur_size = 8;
         }
       ];
 
       input-field = [
         {
-          size = "200, 50";
-          position = "0, -80";
           monitor = "";
-          dots_center = true;
+          size = "20%, 5%";
+          outline_thickness = 3;
+          inner_color = "rgba(0, 0, 0, 0.0)"; # no fill
+
+          outer_color = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
+          fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
+
+          font_color = "rgb(143, 143, 143)";
           fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
-          placeholder_text = ''<i><span foreground="##cad3f5">Password...</span></i>'';
-          shadow_passes = 2;
+          rounding = 15;
+
+          font_family = "$font";
+          placeholder_text = "Input password...";
+          fail_text = "$PAMFAIL";
+
+          # uncomment to use a letter instead of a dot to indicate the typed password
+          # dots_text_format = *
+          # dots_size = 0.4
+          dots_spacing = 0.3;
+
+          # uncomment to use an input indicator that does not show the password length (similar to swaylock's input indicator)
+          # hide_input = true
+
+          position = "0, -20";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      label = [
+        # TIME
+        {
+          monitor = "";
+          text = "$TIME"; # ref. https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#variable-substitution
+          font_size = 90;
+          font_family = "$font";
+
+          position = "-30, 0";
+          halign = "right";
+          valign = "top";
+        }
+
+        # DATE
+        {
+          monitor = "";
+          text = "cmd[update:60000] date +\"%A, %d %B %Y\""; # update every 60 seconds
+          font_size = 25;
+          font_family = "$font";
+
+          position = "-30, -150";
+          halign = "right";
+          valign = "top";
         }
       ];
     };
