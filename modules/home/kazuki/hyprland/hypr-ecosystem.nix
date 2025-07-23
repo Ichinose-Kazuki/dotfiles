fd9c5df14dfadb9631eb673fff797f7e601bfb91
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2025-07-24 00:15:48 +0900

    Disable keyboard backlight control.

--- modules/home/kazuki/hyprland/hypr-ecosystem.nix
+++ modules/home/kazuki/hyprland/hypr-ecosystem.nix
@@ -57,12 +57,6 @@ in
           on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
           on-resume = "brightnessctl -r"; # monitor backlight restore.
         }
-        # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
-        {
-          timeout = 150; # 2.5min.
-          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
-          on-resume = "brightnessctl -rd rgb:kbd_backlight"; # turn on keyboard backlight.
-        }
         {
           timeout = 300; # 5min
           on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
