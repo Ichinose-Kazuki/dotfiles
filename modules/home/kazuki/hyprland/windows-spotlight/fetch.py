6536a54f8e4022b7cad856e1ae9b886b0c5ff9a2
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2025-09-02 01:37:04 +0900

    Keep urls of last three images of windows spotlight.

--- modules/home/kazuki/hyprland/windows-spotlight/fetch.py
+++ modules/home/kazuki/hyprland/windows-spotlight/fetch.py
@@ -1,15 +1,24 @@
 import os
-import requests
 import sys
 
-image_info_response = requests.get(
-    "https://arc.msn.com/v4/api/selection?placement=88000821&fmt=json&locale=ja-JP&country=JP"
-).json()
+import requests
+
+image_info_response = requests.get("https://arc.msn.com/v4/api/selection?placement=88000821&fmt=json&locale=ja-JP&country=JP").json()
 image_url = image_info_response["ad"]["landscapeImage"]["asset"]
 image = requests.get(image_url).content
 
 image_filepath = sys.argv[1]
 os.makedirs(os.path.dirname(image_filepath), exist_ok=True)
+
+try:
+    with open(os.path.join(os.path.dirname(image_filepath), "log.txt"), "r") as f:
+        old = f.readlines()
+except FileNotFoundError:
+    old = []
+with open(os.path.join(os.path.dirname(image_filepath), "log.txt"), "w") as f:
+    old.append(image_url + "\n")
+    # three image urls at most
+    f.writelines(old[1:4] if len(old) > 3 else old)
 
 with open(image_filepath, "wb") as f:
     f.write(image)
