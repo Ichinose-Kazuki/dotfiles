941add92e7c89e45395c4d160f95022f5c4e01d7
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2025-11-07 00:25:56 +0900

    update git and delta options

--- modules/home/kazuki/common/git/default.nix
+++ modules/home/kazuki/common/git/default.nix
@@ -7,23 +7,23 @@
 {
   programs.git = {
     enable = true;
-    userEmail = "ichinose-kazuki657@g.ecc.u-tokyo.ac.jp";
-    userName = "Ichinose-Kazuki";
-    delta.enable = true;
-    aliases = {
-      ci = "commit";
-      cm = "commit -m";
-      cam = "commit -am";
-      co = "checkout";
-      br = "branch";
-      pl = "pull";
-      ps = "push";
-      st = "status";
-      df = "diff";
-      lg = "log";
-    };
-
-    extraConfig = {
+    settings = {
+      alias = {
+        ci = "commit";
+        cm = "commit -m";
+        cam = "commit -am";
+        co = "checkout";
+        br = "branch";
+        pl = "pull";
+        ps = "push";
+        st = "status";
+        df = "diff";
+        lg = "log";
+      };
+      user = {
+        email = "ichinose-kazuki657@g.ecc.u-tokyo.ac.jp";
+        name = "Ichinose-Kazuki";
+      };
       init = {
         defaultBranch = "main";
       };
@@ -41,5 +41,11 @@
         };
       }
     ];
+  };
+
+  # Syntax-highlighting diff viewer
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
   };
 }
