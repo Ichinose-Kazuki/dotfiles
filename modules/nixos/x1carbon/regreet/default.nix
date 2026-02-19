78d76fddbd85adab8648637c2353365a4a2ffcd9
Ichinose-Kazuki <mail@ichinose-kazuki.jp>
2026-02-20 01:08:47 +0900

    regreet: add cageArgs and SESSION_DIRS

--- modules/nixos/x1carbon/regreet/default.nix
+++ modules/nixos/x1carbon/regreet/default.nix
@@ -1,8 +1,19 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
 
 {
   programs.regreet = {
     enable = true;
+    cageArgs = [
+      "-s"
+      "-d"
+      "-m"
+      "last"
+    ];
     theme = {
       package = pkgs.canta-theme;
       name = "";
@@ -12,6 +23,13 @@
         application_prefer_dark_theme = true;
       };
     };
+  };
+
+  environment.sessionVariables = {
+    SESSION_DIRS = [
+      "${config.services.displayManager.sessionData.desktops}/share/wayland-sessions"
+      "${config.services.displayManager.sessionData.desktops}/share/xsessions"
+    ];
   };
 
   # fingerprint auth: https://sbulav.github.io/nix/nix-fingerprint-authentication/
