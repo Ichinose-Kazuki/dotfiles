3429337c7981aae8d0376e1f709063beaae5bea4
Ichinose-Kazuki <mail@ichinose-kazuki.jp>
2026-03-28 01:54:04 +0900

    update flake

--- users/kazuki/home_x1carbon.nix
+++ users/kazuki/home_x1carbon.nix
@@ -38,6 +38,10 @@ in
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
+  # New default behaviors
+  gtk.gtk4.theme = null;
+  xdg.userDirs.setSessionVariables = false;
+  programs.git.signing.format = null;
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
