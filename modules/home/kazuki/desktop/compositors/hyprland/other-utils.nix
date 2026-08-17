57191ee18adff0b450a6d3e211fdb0ac24544fe0
Ichinose-Kazuki <mail@ichinose-kazuki.jp>
2026-08-18 02:02:47 +0900

    remove dup kitty config

--- modules/home/kazuki/desktop/compositors/hyprland/other-utils.nix
+++ modules/home/kazuki/desktop/compositors/hyprland/other-utils.nix
@@ -63,30 +63,6 @@
     ];
   };
 
-  # terminal
-  programs.kitty = {
-    enable = true;
-    shellIntegration = {
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-    };
-    enableGitIntegration = true;
-    # Mappable actions: https://sw.kovidgoyal.net/kitty/actions
-    keybindings = {
-      "ctrl+c" = "copy_or_interrupt";
-      "ctrl+v" = "paste_from_clipboard";
-      "ctrl+equal" = "change_font_size all +2.0";
-      "ctrl+minus" = "change_font_size all -2.0";
-    };
-    # About kitty.conf: https://sw.kovidgoyal.net/kitty/conf
-    settings = {
-      tab_bar_edge = "top";
-      tab_bar_min_tabs = 1;
-      window_padding_width = "5 5";
-    };
-    # run "kitten themes" to preview all themes.
-    themeFile = "Chalk"; # afterglow, bluloco dark, broadcast, chalk,
-  };
   xdg.dataFile."defined_with_nix.profile".text = ''
     [Appearance]
     Font=Hack Nerd Font,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
