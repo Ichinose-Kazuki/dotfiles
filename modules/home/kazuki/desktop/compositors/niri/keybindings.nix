9085b0f3ac0dd4e785835405a35dae6bd62edc85
Ichinose-Kazuki <mail@ichinose-kazuki.jp>
2026-09-09 02:34:02 +0900

    update niri keybindings

--- modules/home/kazuki/desktop/compositors/niri/keybindings.nix
+++ modules/home/kazuki/desktop/compositors/niri/keybindings.nix
@@ -15,6 +15,8 @@ let
       "call"
     ]
     ++ (pkgs.lib.splitString " " cmd);
+  mod = "Super";
+  subMod = "Alt";
 in
 {
   # options: https://github.com/sodiboo/niri-flake/blob/main/docs.md#programsnirisettingsbinds
@@ -23,49 +25,49 @@ in
     lib.mkMerge [
       {
         # pure niri, run `niri msg action help` to see all the actions
-        "Mod+Shift+Q" = {
+        "${mod}+Shift+Q" = {
           action = close-window;
           repeat = false;
         };
 
-        "Mod+K".action = focus-window-or-workspace-up;
-        "Mod+H".action = focus-column-left;
-        "Mod+J".action = focus-window-or-workspace-down;
-        "Mod+L".action = focus-column-right;
+        "${mod}+K".action = focus-window-or-workspace-up;
+        "${mod}+H".action = focus-column-left;
+        "${mod}+J".action = focus-window-or-workspace-down;
+        "${mod}+L".action = focus-column-right;
 
-        "Mod+Shift+K".action = move-column-to-workspace-up;
-        "Mod+Shift+H".action = move-column-left;
-        "Mod+Shift+J".action = move-column-to-workspace-down;
-        "Mod+Shift+L".action = move-column-right;
+        "${mod}+Shift+K".action = move-column-to-workspace-up;
+        "${mod}+Shift+H".action = move-column-left;
+        "${mod}+Shift+J".action = move-column-to-workspace-down;
+        "${mod}+Shift+L".action = move-column-right;
 
         # "".action = focus-monitor-up;
-        "Mod+N".action = focus-monitor-left;
+        "${mod}+N".action = focus-monitor-left;
         # "".action = focus-monitor-down;
-        "Mod+M".action = focus-monitor-right;
+        "${mod}+M".action = focus-monitor-right;
 
         # "".action = move-column-to-monitor-up;
-        "Mod+Shift+N".action = move-column-to-monitor-left;
+        "${mod}+Shift+N".action = move-column-to-monitor-left;
         # "".action = move-column-to-monitor-down;
-        "Mod+Shift+M".action = move-column-to-monitor-right;
+        "${mod}+Shift+M".action = move-column-to-monitor-right;
 
         # Tabbed display or vertical split
-        "Mod+Comma".action = consume-window-into-column;
-        "Mod+Period".action = expel-window-from-column;
-        "Mod+T".action = toggle-column-tabbed-display;
+        "${mod}+Comma".action = consume-window-into-column;
+        "${mod}+Period".action = expel-window-from-column;
+        "${mod}+T".action = toggle-column-tabbed-display;
 
-        "Mod+R".action = switch-preset-column-width;
-        "Mod+Shift+R".action = switch-preset-column-width-back;
-        "Mod+W".action = maximize-column;
-        "Mod+Shift+W".action = fullscreen-window; # Is toggle-windowed-fullscreen better?
+        "${mod}+R".action = switch-preset-column-width;
+        "${mod}+Shift+R".action = switch-preset-column-width-back;
+        "${mod}+W".action = maximize-column;
+        "${mod}+Shift+W".action = fullscreen-window; # Is toggle-windowed-fullscreen better?
 
-        "Mod+O".action = toggle-overview;
+        "${mod}+O".action = toggle-overview;
       }
 
       {
         # noctalia
         # core & navigation
-        "Mod+Space".action.spawn = noctalia "launcher toggle";
-        "Super+V".action.spawn = noctalia "launcher clipboard";
+        "${mod}+Space".action.spawn = noctalia "launcher toggle";
+        "${mod}+${subMod}+V".action.spawn = noctalia "launcher clipboard";
 
         # system controls
         "XF86AudioLowerVolume" = {
@@ -82,22 +84,17 @@ in
         };
 
         # interface & plugins
-        "Super+L".action.spawn = noctalia "lockScreen lock";
+        "${mod}+${subMod}+L".action.spawn = noctalia "lockScreen lock";
       }
 
       {
         # spawn apps
-        "Mod+Shift+T".action.spawn = "kitty";
-        "Mod+Shift+E".action.spawn = "nemo";
-        "Super+Shift+S".action.spawn = [
+        "${mod}+Shift+T".action.spawn = "ghostty";
+        "${mod}+Shift+E".action.spawn = "nemo";
+        "${mod}+Shift+S".action.spawn = [
           "flameshot"
           "gui"
         ];
       }
     ];
-
-  programs.niri.settings.input = {
-    mod-key = "Alt";
-    mod-key-nested = "Super";
-  };
 }
